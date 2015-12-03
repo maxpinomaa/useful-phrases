7ac351a282f898019ca77ef6b75401e4da9931a0
Max Pinomaa <maxpinomaa@hotmail.com>
2015-12-03 20:56:03 +0200

    Useful phrases website thingie, first commit

---
+++ phrasebook/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Convcon
+
+admin.site.register(Convcon)
---
+++ phrasebook/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Convcon(models.Model):
+    author = models.ForeignKey('auth.User')
+    connector = models.CharField(max_length=100)
+    contransl = models.CharField(max_length=100)
+    examplesent = models.CharField(max_length=200)
+    exampletransl = models.CharField(max_length=200)
+    usagenotes = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        return self.connector
+
+
