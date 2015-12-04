1ba0ee21b4e63cff7d0d7c95553c85d68527c96e
Max Pinomaa <maxpinomaa@hotmail.com>
2015-12-04 22:18:30 +0200

    Modified templates to display posts from database.

--- phrasebook/views.py
+++ phrasebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Convcon
 
-# Create your views here.
+def post_list(request):
+    posts = Convcon.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'phrasebook/post_list.html', {'posts': posts})
+
