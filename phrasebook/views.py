6012e8cfa41261d71ae3a6945cd0b5cc38ae8d92
Your Name <maxpinomaa@hotmail.com>
2016-08-02 09:47:23 +0000

    Preparing the sections for other languages.

--- phrasebook/views.py
+++ phrasebook/views.py
@@ -8,7 +8,7 @@ def post_list(request):
     return render(request, 'phrasebook/post_list.html', {'posts': posts})
 
 def reverse_list(request):
-    posts = Convcon.objects.filter(tagi='fin_cq')
+    posts = Convcon.objects.filter(tagi='fin_cq').order_by('-published_date')
     return render(request, 'phrasebook/reverse_list.html', {'posts': posts})
 
 def post_list1(request):
@@ -38,4 +38,38 @@ def post_list6(request):
 def post_list7(request):
     posts = Convcon.objects.filter(tagi='fin_fun').order_by('-published_date')
     return render(request, 'phrasebook/finfunint.html', {'posts': posts})
+
+def post_list8(request):
+    posts = Convcon.objects.filter(audiofilulinkki='/static/Minuntiet.m4a') \
+        .exclude(tagi='fin_cc')
+    return render(request, 'phrasebook/post_list_rus.html', {'posts': posts})
+
+def post_list9(request):
+    posts = Convcon.objects.filter(tagi='rus_fun').order_by('-published_date')
+    return render(request, 'phrasebook/rusfinfun.html', {'posts': posts})
+
+def post_list10(request):
+    posts = Convcon.objects.filter(tagi='ita_cc').order_by('-published_date')
+    return render(request, 'phrasebook/post_list_ita.html', {'posts': posts})
+
+def post_list11(request):
+    posts = Convcon.objects.filter(tagi='spa_cc1').order_by('-published_date')
+    return render(request, 'phrasebook/front_spa.html', {'posts': posts})
+
+def post_list12(request):
+    posts = Convcon.objects.filter(tagi='spa_fun').order_by('-published_date')
+    return render(request, 'phrasebook/espfun.html', {'posts': posts})
+
+def post_list13(request):
+    posts = Convcon.objects.filter(tagi='rus_cq').order_by('-published_date')
+    return render(request, 'phrasebook/reverse_list_rus.html', {'posts': posts})
+
+def post_list14(request):
+    posts = Convcon.objects.filter(tagi='rus_cc2').order_by('-published_date')
+    return render(request, 'phrasebook/finruscc2.html', {'posts': posts})
+
+
+
+
+
 
